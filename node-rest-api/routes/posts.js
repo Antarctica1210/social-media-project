d2b1de26faa205b8eb31b33912ae45514276bb8a
ywang503 <wangyufan940223@gmail.com>
2022-03-25 09:48:43 +1300

    post.js crud

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -6,4 +6,23 @@ router.get("/", (req, res) => {
     res.json("Post page");
 });
 
+//create a post
+
+
+//update a post
+
+
+//delete a post
+
+
+//get a post
+
+
+//get the following's and our posts (with timeline)
+
+
+//like the post
+
+
+
 module.exports = router;
